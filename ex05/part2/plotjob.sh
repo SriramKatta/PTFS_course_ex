9562a8e6b6f628f2ba7c01b74d9a5a1f862475cc
sriram <sriram.katta@fau.de>
2024-06-12 23:21:06 +0200

    update

--- ex05/part2/plotjob.sh
+++ ex05/part2/plotjob.sh
@@ -10,6 +10,7 @@ set grid
 set logscale x
 set xlabel "N"
 set ylabel "GFlops/s"
+set key font ",8"
 
 set xtics rotate by 90 right
 
@@ -17,13 +18,13 @@ set arrow from 2000, graph 0 to 2000, graph 1 nohead
 set arrow from 52083, graph 0 to 52083, graph 1 nohead
 set arrow from 2250000, graph 0 to 2250000, graph 1 nohead
 
-plot './simdata/sim1-dat' title "1 thread" with linespoints, \
-        './simdata/sim2-dat' title "2 thread" with linespoints, \
-        './simdata/sim4-dat' title "4 thread" with linespoints, \
-        './simdata/sim8-dat' title "8 thread" with linespoints, \
-        './simdata/sim12-dat' title "12 thread" with linespoints, \
-        './simdata/sim16-dat' title "16 thread" with linespoints, \
-        './simdata/sim18-dat' title "18 thread" with linespoints, \
+plot './simdata/sim1-dat' title "1 threads" with linespoints, \
+        './simdata/sim2-dat' title "2 threads" with linespoints, \
+        './simdata/sim4-dat' title "4 threads" with linespoints, \
+        './simdata/sim8-dat' title "8 threads" with linespoints, \
+        './simdata/sim12-dat' title "12 threads" with linespoints, \
+        './simdata/sim16-dat' title "16 threads" with linespoints, \
+        './simdata/sim18-dat' title "18 threads" with linespoints, \
         './simdata/simseq-dat' title "no OMP code" with linespoints
 
 
